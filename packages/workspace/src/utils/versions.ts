9e81e315181b1e8dec5116604e7610654945aee1
Jason Jean <jasonjean1993@gmail.com>
2020-05-13 00:06:09 -0400

    test ng 10

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
 
-export const angularCliVersion = '9.1.0';
+export const angularCliVersion = '10.0.0-next.4';
 export const typescriptVersion = '~3.8.3';
 export const prettierVersion = '2.0.4';
 export const typescriptESLintVersion = '2.19.2';
