9ec8543999b8710a6f080dc7de46f8b22fa3589b
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-14 18:00:49 +0300

    feat(misc): add the embeddings to the script

---
+++ scripts/generate-embeddings.ts
@@ -0,0 +1,434 @@
+// based on:
+// https://github.com/supabase-community/nextjs-openai-doc-search/blob/main/lib/generate-embeddings.ts
+
+import { createClient } from '@supabase/supabase-js';
+import { createHash } from 'crypto';
+import dotenv from 'dotenv';
+import { readdir, readFile, stat } from 'fs/promises';
+import GithubSlugger from 'github-slugger';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { toMarkdown } from 'mdast-util-to-markdown';
+import { toString } from 'mdast-util-to-string';
+import 'openai';
+import { Configuration, OpenAIApi } from 'openai';
+import { basename, dirname, join } from 'path';
+import { u } from 'unist-builder';
+import { inspect } from 'util';
+import yargs from 'yargs';
+
+dotenv.config();
+
+/**
+ * Splits a `mdast` tree into multiple trees based on
+ * a predicate function. Will include the splitting node
+ * at the beginning of each tree.
+ *
+ * Useful to split a markdown file into smaller sections.
+ */
+function splitTreeBy(tree: any, predicate: (node: any) => boolean) {
+  return tree.children.reduce((trees: any, node: any) => {
+    const [lastTree] = trees.slice(-1);
+
+    if (!lastTree || predicate(node)) {
+      const tree = u('root', [node]);
+      return trees.concat(tree);
+    }
+
+    lastTree.children.push(node);
+    return trees;
+  }, []);
+}
+
+type Section = {
+  content: string;
+  heading?: string;
+  slug?: string;
+};
+
+type ProcessedMdx = {
+  checksum: string;
+  sections: Section[];
+};
+
+/**
+ * Processes MD content for search indexing.
+ * It extracts metadata and splits it into sub-sections based on criteria.
+ */
+function processMdxForSearch(content: string): ProcessedMdx {
+  const checksum = createHash('sha256').update(content).digest('base64');
+
+  const mdTree = fromMarkdown(content, {});
+
+  if (!mdTree) {
+    return {
+      checksum,
+      sections: [],
+    };
+  }
+
+  const sectionTrees = splitTreeBy(mdTree, (node) => node.type === 'heading');
+
+  const slugger = new GithubSlugger();
+
+  const sections = sectionTrees.map((tree: any) => {
+    const [firstNode] = tree.children;
+
+    const heading =
+      firstNode.type === 'heading' ? toString(firstNode) : undefined;
+    const slug = heading ? slugger.slug(heading) : undefined;
+
+    return {
+      content: toMarkdown(tree),
+      heading,
+      slug,
+    };
+  });
+
+  return {
+    checksum,
+    sections,
+  };
+}
+
+type WalkEntry = {
+  path: string;
+  parentPath?: string;
+};
+
+async function walk(dir: string, parentPath?: string): Promise<WalkEntry[]> {
+  const immediateFiles = await readdir(dir);
+
+  const recursiveFiles = await Promise.all(
+    immediateFiles.map(async (file) => {
+      const path = join(dir, file);
+      const stats = await stat(path);
+      if (stats.isDirectory()) {
+        // Keep track of document hierarchy (if this dir has corresponding doc file)
+        const docPath = `${basename(path)}.md`;
+
+        return walk(
+          path,
+          immediateFiles.includes(docPath)
+            ? join(dirname(path), docPath)
+            : parentPath
+        );
+      } else if (stats.isFile()) {
+        return [
+          {
+            path: path,
+            parentPath,
+          },
+        ];
+      } else {
+        return [];
+      }
+    })
+  );
+
+  const flattenedFiles = recursiveFiles.reduce(
+    (all, folderContents) => all.concat(folderContents),
+    []
+  );
+
+  return flattenedFiles.sort((a, b) => a.path.localeCompare(b.path));
+}
+
+abstract class BaseEmbeddingSource {
+  checksum?: string;
+  sections?: Section[];
+
+  constructor(
+    public source: string,
+    public path: string,
+    public parentPath?: string
+  ) {}
+
+  abstract load(): Promise<{
+    checksum: string;
+    sections: Section[];
+  }>;
+}
+
+class MarkdownEmbeddingSource extends BaseEmbeddingSource {
+  type: 'markdown' = 'markdown';
+
+  constructor(
+    source: string,
+    public filePath: string,
+    public parentFilePath?: string
+  ) {
+    const path = filePath.replace(/^docs/, '').replace(/\.md?$/, '');
+    const parentPath = parentFilePath
+      ?.replace(/^docs/, '')
+      .replace(/\.md?$/, '');
+
+    super(source, path, parentPath);
+  }
+
+  async load() {
+    const contents = await readFile(this.filePath, 'utf8');
+
+    const { checksum, sections } = processMdxForSearch(contents);
+
+    this.checksum = checksum;
+    this.sections = sections;
+
+    return {
+      checksum,
+      sections,
+    };
+  }
+}
+
+type EmbeddingSource = MarkdownEmbeddingSource;
+
+async function generateEmbeddings() {
+  const argv = await yargs.option('refresh', {
+    alias: 'r',
+    description: 'Refresh data',
+    type: 'boolean',
+  }).argv;
+
+  const shouldRefresh = argv.refresh;
+
+  if (!process.env.NX_NEXT_PUBLIC_SUPABASE_URL) {
+    throw new Error(
+      'Environment variable NX_NEXT_PUBLIC_SUPABASE_URL is required: skipping embeddings generation'
+    );
+  }
+
+  if (!process.env.NX_SUPABASE_SERVICE_ROLE_KEY) {
+    throw new Error(
+      'Environment variable NX_SUPABASE_SERVICE_ROLE_KEY is required: skipping embeddings generation'
+    );
+  }
+
+  if (!process.env.NX_OPENAI_KEY) {
+    throw new Error(
+      'Environment variable NX_OPENAI_KEY is required: skipping embeddings generation'
+    );
+  }
+
+  const supabaseClient = createClient(
+    process.env.NX_NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NX_SUPABASE_SERVICE_ROLE_KEY,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+      },
+    }
+  );
+
+  const embeddingSources: EmbeddingSource[] = [
+    ...(await walk('docs'))
+      .filter(({ path }) => /\.md?$/.test(path))
+      .map((entry) => new MarkdownEmbeddingSource('guide', entry.path)),
+  ];
+
+  console.log(`Discovered ${embeddingSources.length} pages`);
+
+  if (!shouldRefresh) {
+    console.log('Checking which pages are new or have changed');
+  } else {
+    console.log('Refresh flag set, re-generating all pages');
+  }
+
+  for (const embeddingSource of embeddingSources) {
+    const { type, source, path, parentPath } = embeddingSource;
+
+    try {
+      const { checksum, sections } = await embeddingSource.load();
+
+      // Check for existing page in DB and compare checksums
+      const { error: fetchPageError, data: existingPage } = await supabaseClient
+        .from('nods_page')
+        .select('id, path, checksum, parentPage:parent_page_id(id, path)')
+        .filter('path', 'eq', path)
+        .limit(1)
+        .maybeSingle();
+
+      if (fetchPageError) {
+        throw fetchPageError;
+      }
+
+      type Singular<T> = T extends any[] ? undefined : T;
+
+      // We use checksum to determine if this page & its sections need to be regenerated
+      if (!shouldRefresh && existingPage?.checksum === checksum) {
+        const existingParentPage =
+          existingPage?.parentPage as unknown as Singular<
+            typeof existingPage.parentPage
+          >;
+
+        // If parent page changed, update it
+        if (existingParentPage?.['path'] !== parentPath) {
+          console.log(
+            `[${path}] Parent page has changed. Updating to '${parentPath}'...`
+          );
+          const { error: fetchParentPageError, data: parentPage } =
+            await supabaseClient
+              .from('nods_page')
+              .select()
+              .filter('path', 'eq', parentPath)
+              .limit(1)
+              .maybeSingle();
+
+          if (fetchParentPageError) {
+            throw fetchParentPageError;
+          }
+
+          const { error: updatePageError } = await supabaseClient
+            .from('nods_page')
+            .update({ parent_page_id: parentPage?.id })
+            .filter('id', 'eq', existingPage.id);
+
+          if (updatePageError) {
+            throw updatePageError;
+          }
+        }
+        continue;
+      }
+
+      if (existingPage) {
+        if (!shouldRefresh) {
+          console.log(
+            `[${path}] Docs have changed, removing old page sections and their embeddings`
+          );
+        } else {
+          console.log(
+            `[${path}] Refresh flag set, removing old page sections and their embeddings`
+          );
+        }
+
+        const { error: deletePageSectionError } = await supabaseClient
+          .from('nods_page_section')
+          .delete()
+          .filter('page_id', 'eq', existingPage.id);
+
+        if (deletePageSectionError) {
+          throw deletePageSectionError;
+        }
+      }
+
+      const { error: fetchParentPageError, data: parentPage } =
+        await supabaseClient
+          .from('nods_page')
+          .select()
+          .filter('path', 'eq', parentPath)
+          .limit(1)
+          .maybeSingle();
+
+      if (fetchParentPageError) {
+        throw fetchParentPageError;
+      }
+
+      // Create/update page record. Intentionally clear checksum until we
+      // have successfully generated all page sections.
+      const { error: upsertPageError, data: page } = await supabaseClient
+        .from('nods_page')
+        .upsert(
+          {
+            checksum: null,
+            path,
+            type,
+            source,
+            parent_page_id: parentPage?.id,
+          },
+          { onConflict: 'path' }
+        )
+        .select()
+        .limit(1)
+        .single();
+
+      if (upsertPageError) {
+        throw upsertPageError;
+      }
+
+      console.log(
+        `[${path}] Adding ${sections.length} page sections (with embeddings)`
+      );
+      for (const { slug, heading, content } of sections) {
+        // OpenAI recommends replacing newlines with spaces for best results (specific to embeddings)
+        const input = content.replace(/\n/g, ' ');
+
+        try {
+          const configuration = new Configuration({
+            apiKey: process.env.NX_OPENAI_KEY,
+          });
+          const openai = new OpenAIApi(configuration);
+
+          const embeddingResponse = await openai.createEmbedding({
+            model: 'text-embedding-ada-002',
+            input,
+          });
+
+          if (embeddingResponse.status !== 200) {
+            throw new Error(inspect(embeddingResponse.data, false, 2));
+          }
+
+          const [responseData] = embeddingResponse.data.data;
+
+          const { error: insertPageSectionError, data: pageSection } =
+            await supabaseClient
+              .from('nods_page_section')
+              .insert({
+                page_id: page.id,
+                slug,
+                heading,
+                content,
+                token_count: embeddingResponse.data.usage.total_tokens,
+                embedding: responseData.embedding,
+              })
+              .select()
+              .limit(1)
+              .single();
+
+          if (insertPageSectionError) {
+            throw insertPageSectionError;
+          }
+
+          // Add delay after each request
+          await delay(500); // delay of 0.5 second
+        } catch (err) {
+          // TODO: decide how to better handle failed embeddings
+          console.error(
+            `Failed to generate embeddings for '${path}' page section starting with '${input.slice(
+              0,
+              40
+            )}...'`
+          );
+
+          throw err;
+        }
+      }
+
+      // Set page checksum so that we know this page was stored successfully
+      const { error: updatePageError } = await supabaseClient
+        .from('nods_page')
+        .update({ checksum })
+        .filter('id', 'eq', page.id);
+
+      if (updatePageError) {
+        throw updatePageError;
+      }
+    } catch (err) {
+      console.error(
+        `Page '${path}' or one/multiple of its page sections failed to store properly. Page has been marked with null checksum to indicate that it needs to be re-generated.`
+      );
+      console.error(err);
+    }
+  }
+
+  console.log('Embedding generation complete');
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  await generateEmbeddings();
+}
+
+main().catch((err) => console.error(err));
