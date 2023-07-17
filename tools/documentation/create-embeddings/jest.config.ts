84e3665c684e5cba16fef202ef3b64a0202ac81a
FrozenPandaz <jasonjean1993@gmail.com>
2023-07-17 11:05:55 -0400

    chore(repo): try using @nx/node to run create-embeddings

---
+++ tools/documentation/create-embeddings/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'tools-documentation-create-embeddings',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/tools/documentation/create-embeddings',
+};
